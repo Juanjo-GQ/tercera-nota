2ade6dd9a165cc44dc9cd48d904cf8ec7f9e3559
Juan-GQ <juanjoquintero08@gmail.com>
2026-09-01 10:16:26 -0500

    Agregado

---
+++ EjercicioPreguntas.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class EjercicioPreguntas{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Condición a comprobar: primer número mayor que el segundo número y menor que el tercer número");
+        System.out.println();
+        System.out.println("Ingrese su primer número");
+        int primerN= entrada.nextInt();
+        System.out.println();
+        System.out.println("Ingrese su segundo número");
+        int segundoN= entrada.nextInt();
+        System.out.println();
+        System.out.println("Ingrese su tercer número");
+        int tercerN= entrada.nextInt();
+        System.out.println();
+
+        boolean comparacion = (primerN>segundoN) && (primerN<tercerN);
+        System.out.println("COMPROBACIÓN--DE--LA--CONDICIÓN");
+        System.out.println("¿Es "+primerN+" mayor a "+segundoN+", y menor a "+tercerN+" ?");
+        System.out.println();
+        if (comparacion){
+            System.out.println("Sí se cumple la condición con los valores ingresados");
+        } else {
+                System.out.println("No se cumple la condición con los valores ingresados");
+        }
+    }
+}
